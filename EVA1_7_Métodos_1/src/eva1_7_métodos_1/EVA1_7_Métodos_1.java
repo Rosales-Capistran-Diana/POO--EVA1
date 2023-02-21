878d149d16ccd8913aa4fcbf9dec5938d1981662
RosCaD <124291679+Rosales-Capistran-Diana@users.noreply.github.com>
2023-02-21 12:37:40 -0600

    Add files via upload

---
+++ EVA1_7_Métodos_1/src/eva1_7_métodos_1/EVA1_7_Métodos_1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_métodos_1;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA1_7_Métodos_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        myMethod();
+    }
+
+    public static void myMethod() {
+        System.out.println("You have called me! My name is myMethod!!");
+    }
+
+}

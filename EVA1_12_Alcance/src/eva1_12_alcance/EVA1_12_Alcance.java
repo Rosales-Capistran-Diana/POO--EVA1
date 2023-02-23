760dfbc8c5259f429a50fdb11d084cbaebc00a0d
RosCaD <124291679+Rosales-Capistran-Diana@users.noreply.github.com>
2023-02-23 12:20:28 -0600

    Add files via upload

---
+++ EVA1_12_Alcance/src/eva1_12_alcance/EVA1_12_Alcance.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_alcance;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA1_12_Alcance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println(x);System.out.println(x);//No existe X porque está declarada después de la instrucción
+        System.out.println(args); // args existe porque está declarda antes de la instrucción.
+        int x = 100;
+       System.out.println(x);System.out.println(x); 
+    }
+    
+}

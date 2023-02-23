47fe51a92d79de189324d334d6173ef3004943c1
RosCaD <124291679+Rosales-Capistran-Diana@users.noreply.github.com>
2023-02-23 12:25:50 -0600

    Add files via upload

---
+++ EVA1_13_Alcance_2/src/eva1_13_alcance_2/EVA1_13_Alcance_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_13_alcance_2;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA1_13_Alcance_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(j);
+            int j = 100;
+            System.out.println(j);
+        }
+        int y = 10;
+        System.out.println(y);
+        
+        System.out.println(i);
+        System.out.println(j);
+    }
+    System.out.println(y);
+}

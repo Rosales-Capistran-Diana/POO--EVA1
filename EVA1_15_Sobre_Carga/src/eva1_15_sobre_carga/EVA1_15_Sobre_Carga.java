a53400b8731d8aa45fb22511302b8f55bb4dcefe
RosCaD <124291679+Rosales-Capistran-Diana@users.noreply.github.com>
2023-02-23 12:47:37 -0600

    Add files via upload

---
+++ EVA1_15_Sobre_Carga/src/eva1_15_sobre_carga/EVA1_15_Sobre_Carga.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_sobre_carga;
+
+/**
+ *
+ * @author 52614
+ */
+public class EVA1_15_Sobre_Carga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+
+    }
+
+    public static void Algo() {
+
+    }
+
+    public static void Algo(int x) {
+
+    }
+}
